b20650b1fdc26c908093764ea3010029ddb0db56
yuhanfeng <hanfeng0416@gmail.com>
2022-08-26 17:39:57 +0800

    fix: 将element-plus全局引入改为按需引入

--- src/App.vue
+++ src/App.vue
@@ -9,16 +9,26 @@
     <el-button type="primary">Primary</el-button>
     <el-button type="success">Primary</el-button>
     <el-button type="danger">Primary</el-button>
+    <div class="flex">
+      <el-button type="primary" :icon="Edit" />
+      <el-button type="primary" :icon="Share" />
+      <el-button type="primary" :icon="Delete" />
+      <el-button type="primary" :icon="Search">Search</el-button>
+      <el-button type="primary">
+        Upload<el-icon class="el-icon--right"><Upload /></el-icon>
+      </el-button>
+    </div>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
+<script setup lang="ts">
+// import { defineComponent } from 'vue'
+import { Delete, Edit, Search, Share, Upload } from '@element-plus/icons-vue'
 
-export default defineComponent({
-  name: 'App',
-  components: {}
-})
+// export default defineComponent({
+//   name: 'App',
+//   components: {}
+// })
 </script>
 
 <style lang="less">
--- vue.config.js
+++ vue.config.js
@@ -1,24 +1,36 @@
-const { defineConfig } = require('@vue/cli-service')
-const path = require('path')
-module.exports = defineConfig({
+// const defineConfig = require('@vue/cli-service')
+const AutoImport = require('unplugin-auto-import/webpack')
+const Components = require('unplugin-vue-components/webpack')
+const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
+// const path = require('path')
+module.exports = {
   transpileDependencies: true,
   outputDir: './build',
-  // configureWebpack: {
-  //   resolve: {
-  //     alias: {
-  //       views: '@/views'
-  //     }
-  //   }
-  // }
+  configureWebpack: {
+    resolve: {
+      alias: {
+        views: '@/views'
+      }
+    },
+    // elementplus 按需引入
+    plugins: [
+      AutoImport({
+        resolvers: [ElementPlusResolver()]
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()]
+      })
+    ]
+  }
   // configureWebpack: (config) => {
   //   config.resolve.alias = {
   //     '@': path.resolve(__dirname, 'src'),
   //     views: '@/views'
   //   }
   // },
-  chainWebpack: (config) => {
-    config.resolve.alias
-      .set('@', path.resolve(__dirname, 'src'))
-      .set('views', '@/views')
-  }
-})
+  // chainWebpack: (config) => {
+  //   config.resolve.alias
+  //     .set('@', path.resolve(__dirname, 'src'))
+  //     .set('views', '@/views')
+  // }
+}
