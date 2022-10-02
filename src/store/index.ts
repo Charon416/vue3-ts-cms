571cdf0b0cabd304d8cf396819fc4b4cac5234c1
hanfeng <hanfeng0416@gmail.com>
2022-10-02 23:13:53 +0800

    fix: 给store中添加system模块

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,6 +3,7 @@ import { createStore, Store, useStore as useVuexStore } from 'vuex'
 import { IRootState, IStoreType } from './types'
 
 import login from './login/login'
+import system from './main/system/system'
 
 const store = createStore<IRootState>({
   state() {
@@ -19,7 +20,8 @@ const store = createStore<IRootState>({
   getters: {},
   actions: {},
   modules: {
-    login
+    login,
+    system
   }
 })
 
