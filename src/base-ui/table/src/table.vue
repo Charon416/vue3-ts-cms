3bef8b8799c0321066e622e046ab9df807bba3a1
hanfeng <hanfeng0416@gmail.com>
2022-10-15 16:21:15 +0800

    fix: 修改base-ui,table的一些东西

--- src/base-ui/table/src/table.vue
+++ src/base-ui/table/src/table.vue
@@ -1,16 +1,63 @@
 <template>
   <div class="hf-table">
-    <el-table :data="listData" border style="width: 100%">
+    <!-- main的头部内容区渲染 -->
+    <div class="header">
+      <slot name="header">
+        <div class="title">{{ title }}</div>
+        <div class="handler">
+          <slot name="headerHandler"></slot>
+        </div>
+      </slot>
+    </div>
+    <el-table
+      :data="listData"
+      border
+      style="width: 100%"
+      @selection-change="handleSelectionChange"
+      v-bind="childrenProps"
+    >
+      <!-- 表格的选择列 -->
+      <el-table-column
+        v-if="showSelectColumn"
+        type="selection"
+        align="center"
+        width="60"
+      ></el-table-column>
+      <!-- 表格的序号列 -->
+      <el-table-column
+        v-if="showIndexColumn"
+        type="index"
+        label="序号"
+        align="center"
+        width="80"
+      ></el-table-column>
+      <!-- 动态渲染列 -->
       <template v-for="propItem in propList" :key="propItem.prop">
         <el-table-column v-bind="propItem" align="center">
           <template #default="scope">
-            <slot :name="propItem.prop" :row="scope.row">
+            <slot :name="propItem.slotName" :row="scope.row">
               {{ scope.row[propItem.prop] }}
             </slot>
           </template>
         </el-table-column>
       </template>
     </el-table>
+    <div class="footer">
+      <slot name="footer">
+        <!-- <el-pagination
+          v-model:currentPage="currentPage4"
+          v-model:page-size="pageSize4"
+          :page-sizes="[100, 200, 300, 400]"
+          :small="small"
+          :disabled="disabled"
+          :background="background"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="400"
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+        /> -->
+      </slot>
+    </div>
   </div>
 </template>
 
@@ -19,18 +66,68 @@ import { defineComponent } from 'vue'
 export default defineComponent({
   name: '',
   props: {
+    // 标题插槽
+    title: {
+      type: String,
+      default: ''
+    },
+    // 数据信息
     listData: {
       type: Array,
       required: true
     },
+    // 属性信息
     propList: {
       type: Array,
       required: true
+    },
+
+    // 是否显示序号列
+    showIndexColumn: {
+      type: Boolean,
+      default: false
+    },
+    // 是否显示选择框列
+    showSelectColumn: {
+      type: Boolean,
+      default: false
+    },
+    // 是否还有子属性
+    childrenProps: {
+      type: Object,
+      default: () => ({})
     }
   },
-  setup() {
-    return {}
+  emits: ['selectionChange'],
+  setup(props, { emit }) {
+    const handleSelectionChange = (value: any) => {
+      emit('selectionChange', value)
+    }
+    return {
+      handleSelectionChange
+    }
   }
 })
 </script>
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.header {
+  display: flex;
+  height: 45px;
+  padding: 0 5px;
+  justify-content: space-between;
+  align-items: center;
+  .title {
+    font-size: 20px;
+    font-weight: 700;
+  }
+  .handler {
+    align-items: center;
+  }
+}
+.footer {
+  margin-top: 15px;
+  .el-pagination {
+    text-align: right;
+  }
+}
+</style>
