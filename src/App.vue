b0ddd54d7b24540f971a1f041e0a3956eb531bd9
hanfeng <hanfeng0416@gmail.com>
2022-10-15 16:18:55 +0800

    feat: 语言国际化

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,31 @@
 <template>
-  <router-view></router-view>
+  <div class="app">
+    <el-config-provider :locale="locale">
+      <router-view></router-view>
+    </el-config-provider>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { ElConfigProvider } from 'element-plus'
 
-<style lang="less"></style>
+import zhCn from 'element-plus/lib/locale/lang/zh-cn'
+export default defineComponent({
+  name: 'App',
+  components: {
+    ElConfigProvider
+  },
+  setup() {
+    return {
+      locale: zhCn
+    }
+  }
+})
+</script>
+
+<style lang="less">
+.app {
+  height: 100%;
+}
+</style>
