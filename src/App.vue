0ecafebc8bbfdf6fc5874f4cfeba66222b74b569
lan <Jaksonkoda1284@gmail.com>
2023-05-13 17:59:31 +0800

    fix

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,8 @@ export default defineComponent({
     ElConfigProvider
   },
   setup() {
+    const arr = [1, 2, 3, 4, 5, 6];
+    const newArr = arr.map(item => item * 2)
     return {
       locale: zhCn
     }
